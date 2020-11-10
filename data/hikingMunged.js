3c4c5d381bade147e6e0a1a81bd16bb9c4074771
Shane Upchurch <shaneupchurch@gmail.com>
2020-11-10 12:29:06 -0600

    working

---
+++ data/hikingMunged.js
@@ -0,0 +1,112 @@
+[
+    {
+        'name': 'Bull of the Woods Lookout',
+        'location': 'Mill City, Oregon',
+        'length': 6.3,
+        'stars': 4.2,
+        'summary': 'Begin in low elevation old-growth forest and climb to a great views from a fire lookout.',
+        'trail_url': 'https://www.hikingproject.com/trail/7011098/bull-of-the-woods-lookout',
+        'conditions': 'Unknown',
+        'condition_date': '1970-01-01',
+        'condition_time': ' 00:00:00'
+    },
+    {
+        'name': 'Schreiner Peak Trail #555',
+        'location': 'Mill City, Oregon',
+        'length': 2,
+        'stars': 5,
+        'summary': 'Hike up big Slide Mountain and down to Lake Lenore on this little-used trail',
+        'trail_url': 'https://www.hikingproject.com/trail/7071165/schreiner-peak-trail-555',
+        'conditions': 'All Clear',
+        'condition_date': '2020-08-04',
+        'condition_time': ' 09:16:39'
+    },
+    {
+        'name': 'Pansy Creek Trail #551',
+        'location': 'Mill City, Oregon',
+        'length': 2,
+        'stars': 4.3,
+        'summary': 'A moderate trail through old growth, past a nice camping lake, and ending at Mother Lode Trail #558 great for loop hikes',
+        'trail_url': 'https://www.hikingproject.com/trail/7055401/pansy-creek-trail-551',
+        'conditions': 'All Clear',
+        'condition_date': '2020-08-04',
+        'condition_time': ' 09:08:04'
+    },
+    {
+        'name': 'Alder Flat Trail #574',
+        'location': 'Mount Hood Village, Oregon',
+        'length': 1,
+        'stars': 4,
+        'summary': 'A short trail through old-growth forest, down to the Clackamas River.',
+        'trail_url': 'https://www.hikingproject.com/trail/7055120/alder-flat-trail-574',
+        'conditions': 'Unknown',
+        'condition_date': '1970-01-01',
+        'condition_time': ' 00:00:00'
+    },
+    {
+        'name': 'Bull of the Woods Trail #550',
+        'location': 'Mill City, Oregon',
+        'length': 4.3,
+        'stars': 4,
+        'summary': 'Needs Summary',
+        'trail_url': 'https://www.hikingproject.com/trail/7055839/bull-of-the-woods-trail-550',
+        'conditions': 'Unknown',
+        'condition_date': '1970-01-01',
+        'condition_time': ' 00:00:00'
+    },
+    {
+        'name': 'Dickey Lake Trail #549',
+        'location': 'Mill City, Oregon',
+        'length': 1.3,
+        'stars': 3,
+        'summary': 'A trail accessing Dickey Lake while connecting Pansy Lake Trail to Bull of the Woods Trail best used for making loops',
+        'trail_url': 'https://www.hikingproject.com/trail/7055837/dickey-lake-trail-549',
+        'conditions': 'All Clear',
+        'condition_date': '2020-08-04',
+        'condition_time': ' 09:09:31'
+    },
+    {
+        'name': 'Bagby Trail #544',
+        'location': 'Mill City, Oregon',
+        'length': 12.5,
+        'stars': 2.7,
+        'summary': 'Needs Summary',
+        'trail_url': 'https://www.hikingproject.com/trail/7071236/bagby-trail-544',
+        'conditions': 'Unknown',
+        'condition_date': '1970-01-01',
+        'condition_time': ' 00:00:00'
+    },
+    {
+        'name': 'Elk Lake Creek Trail #559',
+        'location': 'Mill City, Oregon',
+        'length': 9,
+        'stars': 0,
+        'summary': 'Needs Summary',
+        'trail_url': 'https://www.hikingproject.com/trail/7071144/elk-lake-creek-trail-559',
+        'conditions': 'Unknown',
+        'condition_date': '1970-01-01',
+        'condition_time': ' 00:00:00'
+    },
+    {
+        'name': 'Fish Creek Mountain Trail #541',
+        'location': 'Estacada, Oregon',
+        'length': 2.4,
+        'stars': 0,
+        'summary': 'Needs Summary',
+        'trail_url': 'https://www.hikingproject.com/trail/7071045/fish-creek-mountain-trail-541',
+        'conditions': 'Unknown',
+        'condition_date': '1970-01-01',
+        'condition_time': ' 00:00:00'
+    },
+    {
+        'name': 'Dickey Creek Trail #553',
+        'location': 'Mount Hood Village, Oregon',
+        'length': 6.2,
+        'stars': 0,
+        'summary': 'Needs Summary',
+        'trail_url': 'https://www.hikingproject.com/trail/7071205/dickey-creek-trail-553',
+        'conditions': 'Unknown',
+        'condition_date': '1970-01-01',
+        'condition_time': ' 00:00:00'
+    }
+]
